32b44813522484db6a03481ba5845c0d277ed4c5
Alberto Prieto Löfkrantz <hello@albertoprietolofkrantz.dev>
2024-03-29 11:41:05 +0100

    Initial Commit

---
+++ Training Tracker/Training_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Training_TrackerApp.swift
+//  Training Tracker
+//
+//  Created by Alberto Diego Prieto Löfkrantz on 29/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct Training_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
