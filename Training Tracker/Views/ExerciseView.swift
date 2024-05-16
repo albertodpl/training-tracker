8bd36759a5be345f09d16a255f147c3c71425e57
Alberto Prieto Löfkrantz <hello@albertoprietolofkrantz.dev>
2024-05-16 22:17:15 +0200

    Current exercise shows reps or time

--- Training Tracker/Views/ExerciseView.swift
+++ Training Tracker/Views/ExerciseView.swift
@@ -10,12 +10,40 @@ struct ExerciseView: View {
         self.nextExercise = nextExercise
         self.dim = dim
     }
+    
+    private func formattedTime(durationInSeconds: TimeInterval) -> String {
+        let durationInSecondsTruncated = Int(durationInSeconds)
+        
+        
+        let minutes = durationInSecondsTruncated / 60
+        let seconds = durationInSecondsTruncated % 60
+        return String(format: "%02d:%02d", minutes, seconds)
+    }
+
+    private func formattedReps(repetitions: Int) -> String {
+        return String(format: "x%d", repetitions)
+    }
 
     var body: some View {
         VStack {
             VStack {
-                Text(currentExercise?.name ?? "")
-                    .font(.title)
+                HStack {
+                    if let currentExercise = currentExercise {
+                        switch currentExercise.stepType {
+                        case .timed(let timedStepModel):
+                            Text(formattedTime(durationInSeconds: timedStepModel.definition.durationInSeconds))
+                                .font(.title)
+                        case .reps(let repsStepDefinition):
+                            Text(formattedReps(repetitions: repsStepDefinition.repetitions))
+                                .font(.title)
+                        }
+                        Text(currentExercise.name)
+                            .font(.title)
+                    } else {
+                        Text("")
+                            .font(.title)
+                    }
+                }
                 if let description = currentExercise?.description {
                     Text(description)
                         .font(.title2)
@@ -46,7 +74,7 @@ struct ExerciseView: View {
     return ExerciseView(currentExercise: currentExercise, nextExercise: nextExercise)
 }
 
-#Preview ("Short") {
+#Preview ("Short, dim true") {
     let currentExercise: Step? = Step(name: "Current exercise", description: "Description of the current exercise", stepType: .reps(RepsStepDefinition(repetitions: 8)))
     let nextExercise: Step? = Step(name: "Next exercise", description: "Description of the next exercise", stepType: .reps(RepsStepDefinition(repetitions: 12)))
     return ExerciseView(currentExercise: currentExercise, nextExercise: nextExercise, dim: true)
